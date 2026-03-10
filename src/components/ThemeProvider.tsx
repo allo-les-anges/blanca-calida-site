90bc5d57f5d08bdaa4b12b65fd84870630a79a08
allo-les-anges <gm.harchies@gmail.com>
2026-03-10 12:34:17 +0100

    global.css

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -2,7 +2,15 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes";
+
+// On définit manuellement ce que le composant accepte
+interface ThemeProviderProps {
+  children: React.ReactNode;
+  attribute?: string;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
