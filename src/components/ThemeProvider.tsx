e16c1c188b201b3dd447b89b88d716181fe1462d
allo-les-anges <gm.harchies@gmail.com>
2026-03-10 12:27:42 +0100

    global.css

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,10 +1,9 @@
 "use client";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
-    </NextThemesProvider>
-  );
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
