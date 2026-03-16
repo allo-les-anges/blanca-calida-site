e89fb59a5e87997243858150033ab518db33add4
allo-les-anges <gm.harchies@gmail.com>
2026-03-16 17:09:00 +0100

    update contact page

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -70,6 +70,7 @@ export default function AdminDashboard() {
   // --- ÉTATS POUR LES CONSTATS ---
   const [selectedConstatsDate, setSelectedConstatsDate] = useState<string | null>(null);
   const [isGeneratingPDF, setIsGeneratingPDF] = useState(false);
+  
 
   const [newProject, setNewProject] = useState({ 
     client_nom: "", 
