dede8f2e3383ddeff911c3dc169aa609c10b76ef
allo-les-anges <gm.harchies@gmail.com>
2026-03-26 09:36:06 +0100

    master_light

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // On s'assure que Tailwind ne regarde QUE la classe .dark sur le HTML
   darkMode: 'class',
+  
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./contexts/**/*.{js,ts,jsx,tsx,mdx}",
+    // Si tu as un dossier src/ à la racine, garde la ligne suivante, sinon retire-la
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
